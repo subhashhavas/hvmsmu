5ffa98375bf4736b6a2956879751f9de0a4c2af3
subhash <subhashdamodaran@ecselis.com>
2017-01-16 16:28:24 +0530

    smoobie form custom script

---
+++ js/smoobie_form_script.js
@@ -0,0 +1,56 @@
+var jq$ = jQuery.noConflict();
+
+
+init();
+
+
+
+
+function init()
+{
+	hideFrame('formA1');
+}
+
+
+
+
+
+function showFrame(frameName)
+{
+	switch(frameName)
+	{
+		case 'formA1':
+			 jq$(document).find("input[name='First Name']").prev().show();
+	 		 jq$(document).find("input[name='First Name']").show();		
+
+	 		 jq$(document).find("input[name='Last Name']").prev().show();
+	 		 jq$(document).find("input[name='Last Name']").show();		
+
+	 		 jq$(document).find("input[name='Gender']").prev().show();
+	 		 jq$(document).find("input[name='Gender']").show();		
+
+	 		 jq$(document).find("input[name='User Category']").prev().show();
+	 		 jq$(document).find("input[name='User Category']").show();		
+		break;
+	}
+}
+
+function hideFrame(frameName)
+{
+	switch(frameName)
+	{
+		case 'formA1':
+			 jq$(document).find("input[name='First Name']").prev().hide();
+	 		 jq$(document).find("input[name='First Name']").hide();		
+
+	 		 jq$(document).find("input[name='Last Name']").prev().hide();
+	 		 jq$(document).find("input[name='Last Name']").hide();		
+
+	 		 jq$(document).find("input[name='Gender']").prev().hide();
+	 		 jq$(document).find("input[name='Gender']").hide();		
+
+	 		 jq$(document).find("input[name='User Category']").prev().hide();
+	 		 jq$(document).find("input[name='User Category']").hide();		
+		break;
+	}
+}
